9c1b4dc870bc72d565babf3f42e019f60e8ee115
lipengfei <lipengfei123>
2019-12-28 15:43:00 +0800

    ssm初始化整合

---
+++ src/main/java/com/mmall/ssm/entity/UserInfo.java
@@ -0,0 +1,64 @@
+package com.mmall.ssm.entity;
+
+/**
+ * Created by Administrator on 2019/12/28.
+ */
+public class UserInfo {
+    private int id;
+    private String uname;
+    private int age;
+    private String address;
+    private String email;
+    private String mobile;
+
+    public UserInfo() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}
